c12d71b53ca0470fc7feead4e036e6a0a387ba82
Radek Koblic <90313174+RadeksGalaxy@users.noreply.github.com>
2023-04-12 14:10:04 +0200

    Delete usApp directory

--- usApp/metody.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def datumValidator(y,m,d):
-    try:
-        datum = f'{y}-{m}-{d}'
-        datetime.strptime(datum, '%Y-%m-%d')
-        return True
-    except ValueError:
-        return False
--- usApp/ochrana.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def sql_injection(vstup):
-    regX = '^[^\'\";]*$'  # kontroluje ' a " a ;
-    regX2 = '^(?!.*--).*$'  # kontroluje aby neslo neco zapoznamkovat
-    if not re.match(regX,vstup) or not re.match(regX,str(id)) and not re.match(regX2,vstup) or not re.match(regX2,str(id)):
-        return True
-    else:
-        return False
